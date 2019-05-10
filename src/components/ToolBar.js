100eb742de5e646954fd6efa6e9aa5ce082cc91b
vedanadh <vedanadh.valluru@xceler.ninja>
2019-05-10 17:50:37 +0530

    Humbermenu added.

--- src/components/ToolBar.js
+++ src/components/ToolBar.js
@@ -1,18 +1,46 @@
-import React from 'react'
+import React,{Component} from 'react'
 import './ToolBar.css' 
 import Logo from './assets/log.png'
-const ToolBar = ()=>{
+class ToolBar extends Component{
+     state = {
+        popupshow:false
+    }
+    closepopup = () =>{
+        this.setState({popupshow:false})
+    }
+    openpopup = () =>{
+        this.setState({popupshow:true})
+    }
+  render(){
    return(
+       <div>
        <div className="ToolBar">
          <img src={Logo} alt="Burger-Logo"/>
          <nav> 
              <ul>
-           <li><a href='/'>Burger Builder</a></li>
-           <li><a href='/'>Check Out</a>  </li>
+           <li><a href='/'>Home</a></li>
+           <li><a href='/'>Ingradients</a>  </li>
+           <li style={{float: 'right'}}><a href='/'>Burger Builder</a></li>
+           <li style={{float: 'right'}}><a href='/'>Check Out</a>  </li>
            </ul>
          </nav>
+         
+         </div>
+         <span className="HumburgerMenu"onClick={() => this.openpopup()}><i className="glyphicon glyphicon-menu-hamburger"></i></span>
+         <div className={this.state.popupshow ? "LeftsideBar" : "Close"}>
+         <div className="text-center">
+         <img src={Logo} alt="Burger-Logo"/>
+         </div>
+         <ul>
+           <li><a href='/'>Home</a></li>
+           <li><a href='/'>Ingradients</a>  </li>
+           <li><a href='/'>Burger Builder</a></li>
+           <li ><a href='/'>Check Out</a>  </li>
+           <li  onClick={()=>this.closepopup()}><a style={{cursor:'pointer'}}>X</a></li>
+          </ul>                  
+         </div>
        </div>
-   )
+   )}
 }
 
 export default ToolBar
